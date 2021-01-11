c965e6b97fcac11d2ce7e8c2130f12d008f1379b
euangt <euan.gillespie@outlook.com>
2021-01-11 15:50:12 +0000

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, format: { with: /@/ }
   validates :password_digest, presence: true
+
+  has_secure_password
 end
