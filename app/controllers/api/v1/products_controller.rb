30946f9d60a0a39450e39d176e66da68b8e2253f
euangt <euan.gillespie@outlook.com>
2021-02-01 13:30:01 +0000

    Adds the products create, update and destroy actions

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,26 +1,59 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :check_login, only: [ :create ]
-  def show 
-    render json: Product.find(params[:id])
+  include Paginable
+
+  before_action :set_product, only: %i[show update destroy]
+  before_action :check_login, only: %i[create]
+  before_action :check_owner, only: %i[update destroy]
+
+  def index
+    @products = Product.page(current_page)
+                       .per(per_page)
+                       .search(params)
+
+    options = get_links_serializer_options('api_v1_products_path', @products)
+    options[:include] = [:user]
+
+    render json: ProductSerializer.new(@products, options).serializable_hash
   end
 
-  def index 
-    render json: Product.all
+  def show
+    options = { include: [:user] }
+    render json: ProductSerializer.new(@product, options).serializable_hash
   end
 
-  def create 
+  def create
     product = current_user.products.build(product_params)
-    if product.save 
-      render json: product, status: :created 
-    else 
+    if product.save
+      render json: ProductSerializer.new(product).serializable_hash, status: :created
+    else
       render json: { errors: product.errors }, status: :unprocessable_entity
     end
   end
 
-  private 
+  def update
+    if @product.update(product_params)
+      render json: ProductSerializer.new(@product).serializable_hash
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    head 204
+  end
+
+  private
+
+  def check_owner
+    head :forbidden unless @product.user_id == current_user&.id
+  end
 
   def product_params
     params.require(:product).permit(:title, :price, :published)
   end
-  
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
